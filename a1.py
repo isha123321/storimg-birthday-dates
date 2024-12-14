27dac8d592ec10ffe17f825d46c2a8026d21ea1f
isha123321 <isha101112@hotmail.com>
2024-12-14 17:49:02 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,11 @@
+birthday1 = "january 1, 1990"
+birthday2 = "november 10, 2012"
+birthday3 = "march 20 , 1986"
+birthday4 = "july 14 , 2014"
+birthday5 = "may 30 ,2001"
+
+print("birthday 1:", birthday1)
+print("birthday 2:", birthday2)
+print("birthday 3:", birthday3)
+print("birthday 4:", birthday4)
+print("birthday 5:", birthday5)
